bcbffd206a08c643a3b55459ccf5d1bc98b693c8
CindyYoshita <cindynat11@gmail.com>
2021-08-02 02:10:23 -0300

    MainMenuCindy
    Criação do main menu da cindy

---
+++ Teste_2S_2021/Assets/Scripts/MainMenuCindy/MainMenu.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject firstButton;
+    public GameObject mainMenu;
+    public GameObject optionMenu;
+    public GameObject firstOptionButton;
+    private GameObject lastSelectedGameObject;
+    private GameObject atualSelectedGameObject;
+    
+    public void playGame()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+
+    public void option()
+    {
+        optionMenu.SetActive(true);
+        mainMenu.SetActive(false);
+        EventSystem.current.SetSelectedGameObject((firstOptionButton));
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+        Debug.Log("QUIT");
+    }
+
+    public void Update()
+    {
+        if (GameObject.ReferenceEquals(EventSystem.current.currentSelectedGameObject, null))
+        {
+
+            EventSystem.current.SetSelectedGameObject((firstButton));
+        }
+
+    }
+}
